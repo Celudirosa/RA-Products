7c87c361be1e77e6ba231be90626943edc265d5b
celudirosa <cluqued@live.com>
2024-02-19 16:42:09 +0100

    add get all presentaciones by id metodo

--- src/main/java/com/example/controller/PresentacionController.java
+++ src/main/java/com/example/controller/PresentacionController.java
@@ -7,11 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entities.Presentacion;
+import com.example.entities.Producto;
+import com.example.exception.ResourceNotFoundException;
 import com.example.repository.PresentacionRepository;
+import com.example.repository.ProductoRepository;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,10 +25,10 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class PresentacionController {
 
-    private final ProductoController productoController;
+    private final ProductoRepository productoRepository;
     private final PresentacionRepository presentacionRepository;
 
-    @GetMapping("/tags")
+    @GetMapping("/presentaciones")
     public ResponseEntity<List<Presentacion>> getAllDescripciones() {
         List<Presentacion> presentaciones = new ArrayList<>();
 
@@ -37,5 +41,18 @@ public class PresentacionController {
         return new ResponseEntity<>(presentaciones, HttpStatus.OK);
 
     }
+
+    @SuppressWarnings("null")
+    @GetMapping("/productos/{productoId}/presentaciones")
+    public ResponseEntity<List<Presentacion>> getAllPresentacionesByProductoId(
+            @PathVariable(value = "productoId") Integer productoId) {
+        if (!productoRepository.existsById(productoId)) {
+            throw new ResourceNotFoundException("Not found Tutorial with id = " + productoId);
+        }
+
+        List<Presentacion> presentaciones = presentacionRepository.findPresentaciones   ByProductosId(productoId);
+        return new ResponseEntity<>(presentaciones, HttpStatus.OK);
+    }
+
 
 }
